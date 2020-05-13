fd97af707bb504c24a539564e858f6e5b655dc19
David José Márquez Batiz <djmbdv@gmail.com>
2020-05-13 19:16:30 -0400

    check password and math verification

--- constants/draw-employer.php
+++ constants/draw-employer.php
@@ -47,16 +47,20 @@
 	</div>
 												
 	<div class="col-sm-12 col-md-12">											
-		<div class="form-group"> 
+		<div class="form-group  has-feedback"> 
 			<label>Contraseña</label>
-			<input class="form-control" placeholder="Min 8 y Max 20 caracteres" name="password" required type="password" required> 
+			<input id="password-input" class="form-control" placeholder="Min 8 y Max 20 caracteres" name="password" required type="password" regex="^.{8,20}$" error-msg="Contraseña no v&aacute;lida" required>
+			<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+			<div class="help-block with-errors"></div>
 		</div>											
 	</div>
 													
 	<div class="col-sm-12 col-md-12">											
-		<div class="form-group">
+		<div class="form-group has-feedback">
 			<label>Confirmar contraseña</label>
-			<input class="form-control" placeholder="Repite tu contraseña" name="confirmpassword" required type="password" required> 
+			<input class="form-control" match="password-input" placeholder="Repite tu contraseña" name="confirmpassword" required type="password" error-msg="Las Contraseñas no coinciden" required> 
+			<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+			<div class="help-block with-errors"></div>
 		</div>											
 	</div>								
 	<input type="hidden" name="acctype" value="102">						
@@ -70,6 +74,47 @@
 </div>
 </form>
 <script type="text/javascript">
+	function valide(e){
+		var value =  $(e.srcElement).val();
+		console.log(value);
+		var rg = new RegExp($(e.srcElement).attr("regex"));
+		var errorMsj = $(e.srcElement).attr("error-msg");
+		console.log(rg.test(value));
+		if(!(rg.test(value))){
+			$(e.srcElement).parent(".form-group")
+				.removeClass("has-success").addClass("has-error")
+				.find(".glyphicon").removeClass("glyphicon-ok").addClass("glyphicon-remove");
+			$(e.srcElement).parent(".form-group").find(".help-block").html( $("<p></p>").text(errorMsj));
+			return false;
+		}
+		$(e.srcElement).parent(".form-group")
+				.removeClass("has-error").addClass("has-success")
+				.find(".glyphicon").removeClass("glyphicon-remove").addClass("glyphicon-ok");
+			$(e.srcElement).parent(".form-group").find(".help-block").html( $("<p></p>").text(""));
+		return true;
+	}
+
+	function valideMatch(e){
+		var value =  $(e.srcElement).val();
+		console.log(value);
+		var match = $("#"+$(e.srcElement).attr("match")).val();
+		var errorMsj = $(e.srcElement).attr("error-msg");
+		console.log(match);
+		if(!(value == match)){
+			$(e.srcElement).parent(".form-group")
+				.removeClass("has-success").addClass("has-error")
+				.find(".glyphicon").removeClass("glyphicon-ok").addClass("glyphicon-remove");
+			$(e.srcElement).parent(".form-group").find(".help-block").html( $("<p></p>").text(errorMsj));
+			return false;
+		}
+		$(e.srcElement).parent(".form-group")
+				.removeClass("has-error").addClass("has-success")
+				.find(".glyphicon").removeClass("glyphicon-remove").addClass("glyphicon-ok");
+			$(e.srcElement).parent(".form-group").find(".help-block").html( $("<p></p>").text(""));
+		return true;
+	}
+	$("input[name='password']").keyup(valide);
+	$("input[name='confirmpassword']").keyup(valideMatch);
 	$("input[type='email'][role='register']").keyup(e => {
 		//console.log($(e.srcElement).val());
 		var email = $(e.srcElement).val();
