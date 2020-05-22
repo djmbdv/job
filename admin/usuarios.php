c9b53b242f2a2381b338a7cb9ac5b752163e76c0
David José Márquez Batiz <djmbdv@gmail.com>
2020-05-21 21:39:11 -0400

    endpoint usuarios

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -1,26 +1,38 @@
-<?php 
+<?php
   require_once "../constants/connection.php";
   global $conn;
 
   if(!isset($_GET['cmd'])) die();
-
+  header("Content-type:application/json");
   switch ($_GET['cmd']) {
     case 'list':
       $page = isset($_GET['page'])?intval($_GET['page'])?$_GET['page']:1:1;
       $num_rows = isset($_GET['num'])?intval($_GET['num'])?$_GET['num']:10:10;
-      echo $num_rows;
       $offset = $num_rows * ($page - 1);
-      $stmt = $conn->prepare("select  first_name, last_name, gender, bdate, bmonth, byear, email , education, title, city, street, zip,country, phone, about, services, expertise, people, last_login, role, website, login, member_no, verified from tbl_users  limit $num_rows offset $offset");
+      $stmt = $conn->prepare("select  first_name, last_name, gender, bdate, bmonth, byear, email , education, title, city, street, zip,country, phone, about, services, expertise, people, last_login, role, website,member_no, verified from tbl_users  limit $num_rows offset $offset");
       $stmt->execute();
       $l = [];
-     while($o = $stmt->fetchObject()){
+      while($o = $stmt->fetchObject()){
         array_push($l ,$o );
       }
-      
-      print_r(json_encode($l));
-   
-    
+      echo json_encode($l);
+      break;
+    case 'search':
+      if(!isset($_GET["id"]))break;
+      $id = $_GET['id'];
+      $stmt = $conn->prepare("select  first_name, last_name, gender, bdate, bmonth, byear, email , education, title, city, street, zip,country, phone, about, services, expertise, people, last_login, role, website,member_no, verified from tbl_users where member_no=:id");
+      $stmt->bindParam(":id",$id);
+      $stmt->execute();
+      $o = $stmt->fetch();
+      echo json_encode($o);
+      break;
+    case 'del':
+      if(!isset($_POST["id"]))break;
+      $id = $_GET['id'];
+      $stmt = $conn->prepare("delete from tbl_users where member_no=:id");
+      $stmt->bindParam(":id",$id);
+      echo $stmt->execute();
     default:
-      # code...
+      echo false;
       break;
   }
