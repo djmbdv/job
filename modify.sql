2bb98560dcaaf637d9951f9ae349ddb00a1c0f26
David José Márquez Batiz <djmbdv@gmail.com>
2020-06-16 07:22:22 -0400

    mensajes de alert

--- modify.sql
+++ modify.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `tbl_jobs` CHANGE `closing_date` `closing_date` VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
 ALTER TABLE `tbl_image_service` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+INSERT INTO `tbl_alerts` (`id`, `code`, `description`, `type`) VALUES (NULL, '0446', 'Su correo no ha sido verificado', 'warning'), (NULL, '570', 'Ha verificado su correo con exito!', 'success');
