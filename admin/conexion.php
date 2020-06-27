7d9fe49ef47900d737d818c5acee17d5892ceb2a
simon jose aguilera gomez <simonfilth86@hotmail.com>
2020-06-28 03:49:24 +0800

    Cambio de estilos en la lista de servicios y cambio en la imagen del home

--- admin/conexion.php
+++ admin/conexion.php
@@ -1,7 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "remotepc_job";
-$password = "fabianjose";
+// $username = "remotepc_job";
+$username =  "root";
+// $password = "fabianjose";
 $dbname = "remotepc_bwirejobs_db";
 ///////////////// fork
 $password = "";
